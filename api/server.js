76ab9290a68cd26d6d7b63aed23ee55a803aa980
Yan <yanc@ext.inditex.com>
2024-08-30 12:22:00 +0200

    pubsub mvp

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-const pubSubClient = new WebPubSubServiceClient(process.env.WEB_PUBSUB_CONNECTION_STRING, 'chat');
+const pubSubServiceClient = new WebPubSubServiceClient(process.env.WEB_PUBSUB_CONNECTION_STRING, 'chat');
 
 const groupNames = ['messages', 'notifications'];  // List of groups to join
 
@@ -16,7 +16,7 @@ app.get('/api/chats/negotiate', async (req, res) => {
     const userId = 'your-user-id';  // Get from token
 
     // Generate the client access token
-    const token = await pubSubClient.getClientAccessToken({
+    const token = await pubSubServiceClient.getClientAccessToken({
         userId: userId,
         roles: groupNames.map(groupName => `webpubsub.joinLeaveGroup.${groupName}`)  // Allow the client to join/leave each group
     });
@@ -24,12 +24,13 @@ app.get('/api/chats/negotiate', async (req, res) => {
     res.json({ url: token.url });
 });
 
+// TODO this approach vs pubsubclient?
 app.post('/api/chats/subscribe', async (req, res) => {
     const userId = 'your-user-id';  // Get from token
 
     try {
         for (const groupName of groupNames) {
-            await pubSubClient.group(groupName).addUser(userId);
+            await pubSubServiceClient.group(groupName).addUser(userId);
         }
         res.json({});
     } catch (e) {
@@ -53,7 +54,7 @@ app.post('/api/chats/1/messages', async (req, res) => {
     // get all user ids using chat id and in a loop send messages
     // Send a JSON message
     // First store message in DB
-    await pubSubClient.sendToUser(userId, message, {
+    await pubSubServiceClient.sendToUser(userId, message, {
         filter: `'${groupName}' in groups`,
         messageTtlSeconds: 50
     });
